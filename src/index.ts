cefbe7a70523e0a7179eb75b91e4fe8204cb8b9f
Finneas <85114582+Finneasles@users.noreply.github.com>
2023-04-19 04:42:20 +0100

    :zap: Added primeFactors();

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,26 @@ export function createPhoneNumber(numbers: number[]): string {
     .slice(3, 6)
     .join("")}-${numbers.slice(6, 10).join("")}`;
 }
+
+export const primeFactors = ({
+  n
+}: {
+  n: number;
+}): string | undefined | any => {
+  let factors: string[] = [];
+  let d : number = 2;
+  while (n > 1) {
+    if (n % d === 0) {
+      let p = 0;
+      while (n % d === 0) {
+        p++;
+        n = n / d;
+      }
+      factors.push(`(${d}${p > 1 ? `**${p}` : ''})`);
+    }
+    d++;
+  }
+  return factors.join('');
+};
+
+console.log(primeFactors({ n: 17*17*93*677 }));
